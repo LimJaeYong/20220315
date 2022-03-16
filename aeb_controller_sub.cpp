c56a471114f34abc699a1a61356745de5fa752d9
LimJaeYong <101688100+LimJaeYong@users.noreply.github.com>
2022-03-16 03:51:26 -0700

    Create aeb_controller_sub.cpp

---
+++ aeb_controller_sub.cpp
@@ -0,0 +1,40 @@
+#include "ros/ros.h"
+#include "std_msgs/Bool.h"
+#include "std_msgs/Int8.h"
+#include "std_msgs/String.h"
+#include "sensor_msgs/Range.h"
+#include "geometry_msgs/Twist.h"
+
+std_msgs::Bool flag_AEB;
+
+void UltraSonarCallback(const sensor_msgs::Range::ConstPtr& msg) 
+{
+	ROS_INFO("Soner Seq: [%d]", msg->header.seq);
+	ROS_INFO("Soner Range: [%f]", msg->range);	
+	
+	if(msg->range <= 1.0)
+	{
+		ROS_INFO("AEB_Activated");
+		flag_AEB.data = true;
+	}
+	else {
+		flag_AEB.data = false;
+	}
+}
+
+int main(int argc, char **argv) {
+	int count = 0;
+	
+	ros::init(argc, argv, "aeb_controller_sub");
+	ros::NodeHandle n;
+	ros::Rate loop_rate(10);
+	ros::Subscriber sub = n.subscribe("range", 1000, UltraSonarCallback);
+	
+	while (ros::ok()) {
+		
+		loop_rate.sleep();
+		ros::spinOnce();
+		++count;
+	}
+	return 0;
+}
